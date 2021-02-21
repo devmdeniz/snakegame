da89e78aacb68095752b67c277f81ed33fa72411
devmdeniz <mehmetdeniz1397@gmail.com>
2021-02-21 11:02:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Snake Game</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="css/style.css" />
   </head>
   <body>
     <canvas id="game" class="marginleft" width="400" height="400"></canvas>
