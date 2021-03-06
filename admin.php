a4e5eadb92438223e7441b802e3d9c4aed722567
devmdeniz <mehmetdeniz1397@gmail.com>
2021-03-06 09:33:57 +0300

    Set Leftbar

--- admin.php
+++ admin.php
@@ -8,10 +8,27 @@ session_start();
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Admin Panel</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" media="screen" href="css/admin.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="css/admin.css"/>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
 </head>
 <body>
+
+<div class="flex-container">
+  <div><h3 style = "margin-left: 5px;">Dashboard</h3></div>
+  <div><h3 style = "margin-left: 20px;">Update</h3></div>
+  <div><h3 style = "margin-left: 20px;">Delete</h3></div>  
 </div>
 
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
