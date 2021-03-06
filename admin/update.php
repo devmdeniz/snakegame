57d4cde38dac0a9be87813d5a73039447364e6fb
devmdeniz <mehmetdeniz1397@gmail.com>
2021-03-06 10:43:03 +0300

    Style update.php

--- admin/update.php
+++ admin/update.php
@@ -1,16 +1,13 @@
 <?php
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-</head>
-<body>
 
-<form style = "margin-left: 250px" action = "system/edit.php">
+<head>
+	<link rel="stylesheet" href="css/update.css">
+</head>
+
+
+<form action = "system/edit.php">
   <div class="form-group">
     <label for="exampleInputEmail1">Change Score</label>
     <input type="text" class="form-control" name = "changescore" aria-describedby="emailHelp" placeholder="Enter Score">
@@ -21,6 +18,3 @@
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-
-</body>
-</html>
