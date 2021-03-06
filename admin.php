ab9bfb6190c8812248d6263a432cfa835b4a17ec
devmdeniz <mehmetdeniz1397@gmail.com>
2021-03-06 09:02:55 +0300

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Admin Panel</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" media="screen" href="css/admin.css" />
+</head>
+<body>
+</div>
+
+</body>
+</html>
