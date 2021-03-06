4ee313cc70069be0d3c7cb83990d1438b626584e
devmdeniz <mehmetdeniz1397@gmail.com>
2021-03-06 21:15:46 +0300

    Update delete.php

--- admin/delete.php
+++ admin/delete.php
@@ -3,4 +3,13 @@
 <head>
 	<link rel="stylesheet" href="css/delete.css">
 </head>
-	
+
+
+<form action = "system/delete.php" method = "post">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Delete Score</label>
+	<br>
+	<input pattern="\d*" type="text" placeholder="Score ID" name="deletescore">
+  </div>
+  <button type="submit" class="btn btn-primary">Delete Score</button>
+</form>  
