8b66409607f5abd050db0ef5c2831140012c0fa6
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2021-04-04 16:50:24 +0300

    Update connection.php

--- system/connection.php
+++ system/connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername="sql310.epizy.com";
-$database = "epiz_28206571_scoreboard";
-$username = "epiz_28206571";
-$password = "2qTnOREiKx";
+$servername="localhost";
+$database = "scoreboard";
+$username = "root";
+$password = "";
 
     $conn = mysqli_connect($servername, $username, $password, $database);
     if(!$conn) {
